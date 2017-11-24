76ed3b754ea3b00786f934100d4c813a1df75cb8
mervinonline <33906862+mervinonline@users.noreply.github.com>
2017-11-24 09:17:58 +0000

    Update github.php

--- github.php
+++ github.php
@@ -2,7 +2,7 @@
 
 //define('PRIVATE_KEY', 'XXXXXXXXXXXXXXXXxxx');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST')
-{
+//if ($_SERVER['REQUEST_METHOD'] === 'POST')
+//{
     echo shell_exec("/usr/bin/git pull 2>&1");
-}
+//}
