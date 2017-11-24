5c9db95e8911aa93fd8c2e5f6543df0aa640cc9b
mervinonline <33906862+mervinonline@users.noreply.github.com>
2017-11-24 11:18:08 +0000

    Update github.php

--- github.php
+++ github.php
@@ -1,4 +1,4 @@
-<?php echo shell_exec('/usr/bin/git pull 2>&1');
+<?php echo shell_exec('/usr/bin/git pull https://github.com/mervinonline/test.git master 2>&1');
 
 //define('PRIVATE_KEY', 'XXXXXXXXXXXXXXXXxxx');
 
